ab3bb485557d7eccc080d013aae83b6859f848b2
Artsiom <Frt@mail.ru>
2023-01-15 19:44:43 +0300

    Решение задачи 2 урок 9

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 2: Задайте значения M и N.
+ Напишите программу, которая 
+ найдёт сумму натуральных элементов в промежутке от M до N.
+ Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = M;
+int summ  = M;
+int RecursionF(int n)
+{
+    if (count == N)
+    {
+        
+        return summ;
+    }
+    else
+    {
+        count++;
+        summ += count;
+        return RecursionF(count);
+    }
+}
+Console.WriteLine(RecursionF(N));
